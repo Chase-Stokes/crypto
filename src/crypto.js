d593fa4575328db7e9c989a29c5526ba8dd9b566
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-10 15:56:59 -0800

    make additional api call to display a chart on the webpage, add additional styling
    Co-authored-by: Will Greenberg <greenberg.will@gmail.com>
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- src/crypto.js
+++ src/crypto.js
@@ -10,16 +10,15 @@ export default class CryptoAPI {
       return error.message;
     }
   }
-
-//   static async getExchange() {
-//     try {
-//       const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&interval=1d,30d&per-page=10&page=1`);
-//       if (!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       return response.json();
-//     } catch(error) {
-//       return error.message;
-//     }
-//   }
+  static async valueOverTime() {
+    try {
+      const response = await fetch(`https://api.nomics.com/v1/exchange-rates/history?key=${process.env.API_KEY}&currency=BTC&start=2018-01-01T00%3A00%3A00Z&end=2022-02-10T00%3A00%3A00Z`);
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import Chart from 'chart.js/auto';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CryptoAPI from './crypto.js';
+
 let currencies = [];
 
 function getElements(response) {
   if(response[0].id) {
+    $("#box1").append(`<h2 class="cryptoCurrencies">Top Cryptocurrencies by Market Cap</h2>`);
     for (let i = 0; i < 10; i ++) {
-      $("#box1").append(`<div><img src='${response[i].logo_url}'> <span class='name'>${response[i].name}</span>, <span class='blue'>ID</span>: ${response[i].id}, <span class='green'>Price:</span> $${Math.floor((response[i].price*1000))/1000}, <span class='red'>High:</span> $${Math.floor((response[i].high)*100)/100}</div>`);
+      $("#box1").append(`<div><img src='${response[i].logo_url}'> <span class='name'>${response[i].name}</span>, <span class='blue'>ID</span>: ${response[i].id}, <span class='green'>Price:</span> $${Math.floor((response[i].price*1000))/1000}, <span class='rank'>Rank:</span> ${response[i].rank} <span class='high'>High:</span> $${Math.floor((response[i].high)*100)/100}</div>`);
       currencies.push(response[i].price);
     } 
   } else {
@@ -23,15 +26,40 @@ function exchange(currency1, currency2) {
   $('#exchange-output').text(`The exchange rate is ${output}`);
 }
 
-async function makeApiCall() {
-  const response = await CryptoAPI.getCurrencyAndPrice();
-  getElements(response);
+function displayChart(responseOT) {
+  const xValues = [];
+  const yValues = [];
+  for(let i=1; i <1501; i++) {
+    xValues.push(i);
+    yValues.push(responseOT[i].rate);
+  }
+  new Chart("chart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(255,0,0,1.0)",
+        borderColor: "rgba(0,0,255,0.1)",
+        data: yValues
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        yAxes: [{ticks: {beginAtZero: true}}],
+      }
+    }
+  });
 }
 
-// async function makeSecondCall(currency) {
-//   const response = await CryptoAPI.getExchange();
-//   exchange(response, currency);
-// }
+async function makeApiCall() {
+  const response = await CryptoAPI.getCurrencyAndPrice();
+  const responseOT = await CryptoAPI.valueOverTime();
+  getElements(response);
+  displayChart(responseOT);
+}
 
 $('document').ready(function(){
   makeApiCall();
