bd1714927de9f215dbda5cdc4f5930231443c8f9
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-09 16:05:34 -0800

    refactor js and add further styling
    Co-authored-by: Will Greenberg <greenberg.will@gmail.com>
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- src/crypto.js
+++ src/crypto.js
@@ -11,15 +11,15 @@ export default class CryptoAPI {
     }
   }
 
-  static async getExchange() {
-    try {
-      const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&interval=1d,30d&per-page=10&page=1`);
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    } catch(error) {
-      return error.message;
-    }
-  }
+//   static async getExchange() {
+//     try {
+//       const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&interval=1d,30d&per-page=10&page=1`);
+//       if (!response.ok) {
+//         throw Error(response.statusText);
+//       }
+//       return response.json();
+//     } catch(error) {
+//       return error.message;
+//     }
+//   }
 }
--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CryptoAPI from './crypto.js';
+let currencies = [];
 
 function getElements(response) {
-  if(response) {
+  if(response[0].id) {
     for (let i = 0; i < 10; i ++) {
       $("#box1").append(`<div><img src='${response[i].logo_url}'> <span class='name'>${response[i].name}</span>, <span class='blue'>ID</span>: ${response[i].id}, <span class='green'>Price:</span> $${Math.floor((response[i].price*1000))/1000}, <span class='red'>High:</span> $${Math.floor((response[i].high)*100)/100}</div>`);
+      currencies.push(response[i].price);
     } 
   } else {
     $(".showErrors").text(`There was an error: ${response}`);
   }
 }
 
-
-function exchange(response, currency) {
-  let output = ((response[0].price) / response[currency].price);
-  if (response) {
-    $('#exchange-output').text(Math.floor(output*100)/100+" "+response[currency].name);
-  } else {
-    $(".showErrors").text(`There was an error: ${response}`);
-  }
+function exchange(currency1, currency2) {
+  let num1 = currencies[currency1];
+  let num2 = currencies[currency2];
+  let output = num1 / num2;
+  $('#exchange-output').text(`The exchange rate is ${output}`);
 }
 
 async function makeApiCall() {
@@ -29,16 +28,16 @@ async function makeApiCall() {
   getElements(response);
 }
 
-async function makeSecondCall(currency) {
-  const response = await CryptoAPI.getExchange();
-  exchange(response, currency);
-}
+// async function makeSecondCall(currency) {
+//   const response = await CryptoAPI.getExchange();
+//   exchange(response, currency);
+// }
 
 $('document').ready(function(){
   makeApiCall();
-  $("#exchange-btn").on("click", function(event){
-    event.preventDefault();
-    let currency = parseInt($("#currency").val());
-    makeSecondCall(currency);
+  $("#exchange-btn").on("click", function(){
+    let currency1 = parseInt($("#choice1").val());
+    let currency2 = parseInt($("#choice2").val());
+    exchange(currency1, currency2);
   });
 });
